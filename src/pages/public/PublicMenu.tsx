68506d7a0b06360bfc366ad2c3c85bd369245d4f
TeipeAdmin <admin@teipedigital.com>
2025-10-17 20:59:32 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -308,7 +308,7 @@ export const PublicMenu: React.FC = () => {
                 {cartItemsCount > 0 && (
                   <span
                     className="absolute -top-1 -right-1 w-5 h-5 rounded-full flex items-center justify-center text-xs font-bold text-white"
-                    style={{ backgroundColor: primaryColor }}
+                    style={{ backgroundColor: secondaryColor }}
                   >
                     {cartItemsCount}
                   </span>
