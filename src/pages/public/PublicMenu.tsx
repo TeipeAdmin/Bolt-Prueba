597879f1c849bc5e20f1059124b593df7f5be658
TeipeAdmin <admin@teipedigital.com>
2025-10-20 19:21:45 -0500

    Updated PublicMenu.tsx

--- src/pages/public/PublicMenu.tsx
+++ src/pages/public/PublicMenu.tsx
@@ -397,7 +397,9 @@ export const PublicMenu: React.FC = () => {
                       onClick={() => isCenter && setSelectedProduct(product)}
                     >
                       <div className="relative flex flex-col items-center rounded-2xl shadow-md p-4  "> 
-
+                            style={{
+                              backgroundColor: cardBackgroundColor,
+                            }}
                         <img
                           src={product.images[0]}
                           alt={product.name}
