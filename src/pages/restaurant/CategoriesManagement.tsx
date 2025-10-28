bde83e2973a794c2e6bc00be682bb6b98f4dd8e0
TeipeAdmin <admin@teipedigital.com>
2025-10-28 12:18:00 -0500

    Updated CategoriesManagement.tsx

--- src/pages/restaurant/CategoriesManagement.tsx
+++ src/pages/restaurant/CategoriesManagement.tsx
@@ -355,7 +355,7 @@ export const CategoriesManagement: React.FC = () => {
           )}
         </div>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-4">
           {filteredCategories.map((category, index) => (
             <div
               key={category.id}
