1d959439bee14391f276426069adda8dace2c749
TeipeAdmin <admin@teipedigital.com>
2025-10-29 13:24:06 -0500

    Updated SubscriptionsManagement.tsx

--- src/pages/superadmin/SubscriptionsManagement.tsx
+++ src/pages/superadmin/SubscriptionsManagement.tsx
@@ -437,7 +437,7 @@ export const SubscriptionsManagement: React.FC = () => {
                       {subscription.duration === 'annual' && 'Anual'}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                      {new Date(subscription.end_date).toLocaleDateString()}
+                      {new Date(subscription.end_date +1).toLocaleDateString()}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm font-medium">
                       <div className="flex items-center gap-2">
