f9bbd584462bbdc2951b9f3051ab9bf5e44e1e57
TeipeAdmin <admin@teipedigital.com>
2025-10-30 13:33:44 -0500

    Updated Toast.tsx

--- src/components/ui/Toast.tsx
+++ src/components/ui/Toast.tsx
@@ -45,7 +45,7 @@ export const Toast: React.FC<ToastProps> = ({
   const getStyles = () => {
     if (customColors?.primary) {
       return {
-      backgroundColor: `${customColors.primary}10`,
+      backgroundColor: `${customColors.primary}50`,
         borderColor: `${customColors.primary}40`,
         color: customColors.secondary || '#FFFFF'
       };
