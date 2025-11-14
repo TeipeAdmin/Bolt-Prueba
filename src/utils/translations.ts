08c32d6790ddc0022ae49efa266e77514ff72d6d
TeipeAdmin <admin@teipedigital.com>
2025-11-14 15:16:33 -0500

    Updated translations.ts

--- src/utils/translations.ts
+++ src/utils/translations.ts
@@ -4,7 +4,7 @@ export type Language = 'es' | 'en';
 interface Translations {
   // Common
   save: string;
-  cancel: string;
+  cancel: string;a
   edit: string;
   delete: string;
   create: string;
