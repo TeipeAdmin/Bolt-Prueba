57eb767b86035b77f26a3b709ba1087375aae744
TeipeAdmin <admin@teipedigital.com>
2025-11-14 15:11:18 -0500

    Updated LanguageContext.tsx

--- src/contexts/LanguageContext.tsx
+++ src/contexts/LanguageContext.tsx
@@ -11,13 +11,13 @@ interface LanguageContextType {
 
 const LanguageContext = createContext<LanguageContextType | undefined>(undefined);
 
-export const useLanguage = () => {
+export function useLanguage() {
   const context = useContext(LanguageContext);
   if (context === undefined) {
     throw new Error('useLanguage must be used within a LanguageProvider');
   }
   return context;
-};
+}
 
 interface LanguageProviderProps {
   children: ReactNode;
